11381445ba5cd3b6720da88764ed145b2a9af44c
Ubuntu <ubuntu@ip-172-31-54-111.ec2.internal>
2021-09-20 15:09:22 +0000

    newtesting_20sep

--- variables.tf
+++ variables.tf
@@ -2,9 +2,11 @@
 variable "aws_region" {
   description = "Region in which AWS Resources to be created"
   type = string
-  }
+}
 
 variable "instance_type" {
   description = "EC2 Instance Type - Instance Sizing"
   type = string
 }
+
+### new testing 20 september.
